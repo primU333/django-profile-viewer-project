8ee96f9e151dd2330d38916ba44b76767478db68
primU33 <primu333@gmail.com>
2022-02-13 01:19:40 +0300

    ...

--- django_profile_viewer/django_profile_viewer/wsgi.py
+++ django_profile_viewer/django_profile_viewer/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_profile_viewer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_profile_viewer.settings.dev')
 
 application = get_wsgi_application()
